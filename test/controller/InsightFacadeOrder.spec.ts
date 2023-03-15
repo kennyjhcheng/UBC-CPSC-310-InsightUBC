9ab65ea32f38081aa5ca273768f22946c8443d26
kennyjhcheng <kennyjiahuacheng@gmail.com>
2023-03-15 11:25:29 -0700

    added failing tests for checking exact order with ORDER and TRANSFORMATIONS

---
+++ test/controller/InsightFacadeOrder.spec.ts
@@ -0,0 +1,62 @@
+import InsightFacade from "../../src/controller/InsightFacade";
+import {InsightDatasetKind, InsightError, ResultTooLargeError,} from "../../src/controller/IInsightFacade";
+import {clearDisk, getContentFromArchives} from "../TestUtil";
+import chai, {expect} from "chai";
+import chaiAsPromised from "chai-as-promised";
+import {folderTest} from "@ubccpsc310/folder-test";
+import {before} from "mocha";
+
+/**
+ * Tests for new ORDER EBNF
+ */
+chai.use(chaiAsPromised);
+export default function OrderPerformQueryTests() {
+	let rooms: string;
+	let sections: string;
+	let facade: InsightFacade;
+
+	before(function () {
+		rooms = getContentFromArchives("rooms.zip");
+		sections = getContentFromArchives("pair.zip");
+	});
+	describe("performQuery - order and transformation tests", async () => {
+		before(async () => {
+			clearDisk();
+			facade = new InsightFacade();
+			await facade.addDataset("rooms", rooms, InsightDatasetKind.Rooms);
+			await facade.addDataset("sections", sections, InsightDatasetKind.Sections);
+		});
+		type Error = "InsightError" | "ResultTooLargeError";
+		type Output = any[];
+		type Input = any;
+		function errorValidator(error: any): error is Error {
+			return error === "ResultTooLargeError" || error === "InsightError";
+		}
+
+		function assertOnError(actual: any, expected: Error): void {
+			if (expected === "InsightError") {
+				expect(actual).to.be.instanceof(InsightError);
+			} else if (expected === "ResultTooLargeError") {
+				expect(actual).to.be.instanceof(ResultTooLargeError);
+			} else {
+				// this should be unreachable
+				expect.fail("UNEXPECTED ERROR");
+			}
+		}
+
+		function assertOnResult(actual: unknown, expected: Output): void {
+			expect(actual).to.have.deep.equals(expected);
+		}
+
+		folderTest<Input, Output, Error>(
+			"Add Dynamic queries for perform query",
+			(input) => facade.performQuery(input),
+			"./test/resources/queries/order",
+			{
+				errorValidator,
+				assertOnError,
+				assertOnResult,
+			}
+		);
+	});
+}
