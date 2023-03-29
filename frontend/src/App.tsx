6db7918a26fee923eaaf737aaa5802f9215a1ec9
Dhrubo <dhruv.karmaker@gmail.com>
2023-03-29 16:52:05 -0700

    added user story 2 ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import Button from '@mui/material/Button';
 import QueryHistoricAverage from "./QueryHistoricAverage/QueryHistoricAverage";
+import RoomQuery from "./QueryHistoricAverage/RoomQuery";
+import {Divider} from "@mui/material";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
 		  <QueryHistoricAverage></QueryHistoricAverage>
+          <Divider />
+          <RoomQuery></RoomQuery>
       </header>
     </div>
   );
